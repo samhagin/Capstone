96178851253ea7c72bb66a5ff9a4cbf1747f0ea8
Sam Hagin <samhagin@yahoo.com>
2023-02-20 08:38:37 -0700

    made charting changes
    removed charting based on data from polygon.io and added tradingview instead

--- app.py
+++ app.py
@@ -43,88 +43,94 @@ def ticket(symbol):
     limit = 30  # The number of data points to retrieve
     end_date = date.today().strftime('%Y-%m-%d')  # Today's date in the required format
 
-    # Make the API request
-    url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/{interval}/2022-01-01/{end_date}?limit={limit}&apiKey={api_key}'
-    response = requests.get(url)
-    print( response.status_code )
-
-    # Extract the data from the API response
-    data = response.json()['results']
-
-    # Extract the timestamps and closing prices from the data
-    timestamps = [datapoint['t'] for datapoint in data]
-    prices = [datapoint['c'] for datapoint in data]
-
-    # Convert the timestamps to datetime objects
-    dates = [datetime.fromtimestamp(timestamp / 1000) for timestamp in timestamps]
-
-    # Create a Plotly line chart
-    fig = go.Figure(data=go.Scatter(x=dates, y=prices))
-    fig.update_layout(title=symbol, xaxis_title='Date', yaxis_title='Closing Price')
-
-    # Format the date labels as desired
-    date_format = '%m/%d/%Y'
-    fig.update_layout(xaxis_tickformat=date_format)
-
-    # Convert the chart to JSON format and pass it to the template
-    chart_data = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
     # generate buy/sell recommendations using tradingview ta
-    # tv = TA_Handler(
-    # symbol=symbol,
-    # screener="america",
-    # exchange="NASDAQ",
-    # interval=Interval.INTERVAL_5_MINUTES
-    # )
+    recommendation = ''
+    indicators = ''
+    try:
+        tv = TA_Handler(
+        symbol=symbol,
+        screener="america",
+        exchange="NASDAQ",
+        interval=Interval.INTERVAL_5_MINUTES
+        )
+        
+        recommendation = tv.get_analysis().summary['RECOMMENDATION'].replace('_',' ')
+        indicators = tv.get_indicators()
 
-    # recommendation = tv.get_analysis().summary['RECOMMENDATION'].replace('_',' ')
-    # indicators = tv.get_indicators()
-
-    # # relevant tech analysis
-    # #rsi = indicators['RSI']
-    # #stoch.k = indicators['Stoch.K']
-    # #stock.d = indicators['Stoch.D']
+        #relevant tech analysis
+        rsi = indicators['RSI']
+        stochk = indicators['Stoch.K']
+        stockd = indicators['Stoch.D']
+    except:
+        recommendation = 'Unable to find ticker on Tradingview'
     
 
     # calculate ema crossover
     url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2023-01-01/{end_date}?limit=30&apiKey={api_key}' 
-
-    response = requests.get( url )
-    data = response.json()['results']
-
-    # Convert response to pandas dataframe and set timestamp as index
-    df = pd.DataFrame(data)
-    df["timestamp"] = pd.to_datetime(df["t"], unit="ms")
-    df.set_index("timestamp", inplace=True)
-
-    # Calculate 13 and 48 EMA
-    df["ema13"] = df["c"].ewm(span=13).mean()
-    df["ema48"] = df["c"].ewm(span=48).mean()
-
-    # Check for crossover
+    
+    data = ''
     recommendation = ''
-    last_ema13 = df["ema13"].iloc[-1]
-    last_ema48 = df["ema48"].iloc[-1]
-    if last_ema13 > last_ema48:
-        recommendation = "Bullish crossover!, BUY"
-    elif last_ema13 < last_ema48:
-        recommendation = "Bearish crossover!, SELL"
-    else:
-         recommendation = "No crossover."
-
     color = ''
-    if 'SELL' in recommendation:
-        color = 'red'
-    else:
-        color = 'green'
+    try:
+        response = requests.get( url )
+        if response.status_code == 200:
+            data = response.json().get('results') 
+
+             # Convert response to pandas dataframe and set timestamp as index
+            df = pd.DataFrame(data)
+            df["timestamp"] = pd.to_datetime(df["t"], unit="ms")
+            df.set_index("timestamp", inplace=True)
+
+            # Calculate 13 and 48 EMA
+            df["ema13"] = df["c"].ewm(span=13).mean()
+            df["ema48"] = df["c"].ewm(span=48).mean()
+
+            # Check for crossover
+            recommendation = ''
+            last_ema13 = df["ema13"].iloc[-1]
+            last_ema48 = df["ema48"].iloc[-1]
+            if last_ema13 > last_ema48:
+                recommendation = "Bullish crossover!, BUY"
+            elif last_ema13 < last_ema48:
+                recommendation = "Bearish crossover!, SELL"
+            else:
+                 recommendation = "No crossover."
+
+            if 'SELL' in recommendation:
+                color = 'red'
+            else:
+                color = 'green'
+    except KeyError as ke:
+         data = 'Unable to fetch data for this ticker', ke
+
+    
+    # tradingview chart url
+    exchange = 'NYSE'
+    tv_url = f'https://www.tradingview.com/symbols/{exchange}-{symbol}/'
+    r = requests.get( tv_url )
+    if r.status_code != 200:
+        exchange = 'NASDAQ'
+        tv_url = f'https://www.tradingview.com/symbols/{exchange}-{symbol}/'
 
     
     # fetch news
-    sql = "select company.company_id, article, article_date, article_link  from company inner join news on company.company_id = news.company_id;"
+    with connection.cursor() as cursor:
+        sql = 'select * from ticker where symbol = %s'
+        cursor.execute(sql, symbol )
+        result = cursor.fetchone()
+        company_id = result['company_id']
 
+        sql = "select article, article_date, article_link from news where company_id = %s"
+        cursor.execute( sql, company_id )
+        result = cursor.fetchall()
+
+
+    # fetch earnings
+
+        
 
     # Render the template and pass the chart data to it
-    return render_template('ticker.html', chart_data=chart_data, recommendation=recommendation, color=color )
+    return render_template('ticker.html', recommendation=recommendation, color=color, data=result, info=indicators, ticker=symbol, tv_url=tv_url, exchange=exchange)
 
 
 #@app.errorhandler(Exception)
