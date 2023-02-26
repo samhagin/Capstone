dcbf0f5b23cecb60a78840a5278d719922f253d4
Sam Hagin <samhagin@yahoo.com>
2023-02-26 02:13:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, Response, render_template, request, redirect, url_for, session
+from flask import Flask, Response, render_template, request, redirect, url_for, session, flash
 import pymysql
 import configparser
 import plotly.graph_objs as go
-import yfinance as yf
-import matplotlib.pyplot as plt
 from datetime import datetime, date, timedelta
-from io import BytesIO
 import base64, requests, json, plotly, re
 from tradingview_ta import TA_Handler, Interval, Exchange
+from werkzeug.security import generate_password_hash, check_password_hash
 import pandas as pd
+from textblob import TextBlob
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -16,7 +15,7 @@ config.read('config.ini')
 api_key = config['Polygon']['api_key']
 
 app = Flask(__name__)
-
+app.secret_key = '2rzpXVhAidTwbMKM0yhTlciRmpBOiz9J'
 
 # Connect to the database
 connection = pymysql.connect(
@@ -30,11 +29,20 @@ connection = pymysql.connect(
 @app.route("/")
 def index():
     # Execute a SELECT statement
+    ticker = ''
+    result = ''
+    print( session, flush=True )
     with connection.cursor() as cursor:
-        sql = "select company.company_id, name, symbol, info_url  from company inner join ticker on company.company_id = ticker.company_id;"
+        sql = "select * from ticker_list"
         cursor.execute(sql)
         result = cursor.fetchall()
-    return render_template('dashboard.html', data=result)
+        cursor.close()
+
+    # check change in price
+    for i in result:
+        url = f'https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/{ticker}?apiKey={api_key}'
+        
+    return render_template('index.html', data=result)
 
 @app.route('/ticker/<symbol>')
 def ticket(symbol): 
@@ -43,37 +51,32 @@ def ticket(symbol):
     limit = 30  # The number of data points to retrieve
     end_date = date.today().strftime('%Y-%m-%d')  # Today's date in the required format
 
-    # generate buy/sell recommendations using tradingview ta
-    recommendation = ''
-    indicators = ''
-    try:
-        tv = TA_Handler(
-        symbol=symbol,
-        screener="america",
-        exchange="NASDAQ",
-        interval=Interval.INTERVAL_5_MINUTES
-        )
-        
-        recommendation = tv.get_analysis().summary['RECOMMENDATION'].replace('_',' ')
-        indicators = tv.get_indicators()
+    # fetch news
+    company_id = ''
+    with connection.cursor() as cursor:
+        sql = 'select * from ticker where symbol = %s'
+        cursor.execute(sql, symbol )
+        result = cursor.fetchone()
+        company_id = result['company_id']
 
-        #relevant tech analysis
-        rsi = indicators['RSI']
-        stochk = indicators['Stoch.K']
-        stockd = indicators['Stoch.D']
-    except:
-        recommendation = 'Unable to find ticker on Tradingview'
+        sql = "select article, article_date, article_link from news where company_id = %s"
+        cursor.execute( sql, company_id )
+        result = cursor.fetchall()
+        cursor.close()
     
 
     # calculate ema crossover
-    url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2023-01-01/{end_date}?limit=30&apiKey={api_key}' 
+    url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/5/minute/2023-01-01/{end_date}?limit=30&apiKey={api_key}' 
     
-    data = ''
-    recommendation = ''
-    color = ''
-    try:
-        response = requests.get( url )
-        if response.status_code == 200:
+    data = '' 
+    recommendation = '' 
+    color = '' 
+    last_ema13 = ''
+    last_ema48 = '' 
+    ema_direction = ''
+    try: 
+        response = requests.get( url ) 
+        if  response.status_code == 200: 
             data = response.json().get('results') 
 
              # Convert response to pandas dataframe and set timestamp as index
@@ -96,12 +99,9 @@ def ticket(symbol):
             else:
                  recommendation = "No crossover."
 
-            if 'SELL' in recommendation:
-                color = 'red'
-            else:
-                color = 'green'
     except KeyError as ke:
          data = 'Unable to fetch data for this ticker', ke
+    print( recommendation, flush=True )
 
     
     # tradingview chart url
@@ -113,25 +113,113 @@ def ticket(symbol):
         tv_url = f'https://www.tradingview.com/symbols/{exchange}-{symbol}/'
 
     
-    # fetch news
+    # fetch company about text
+    about = ''
     with connection.cursor() as cursor:
-        sql = 'select * from ticker where symbol = %s'
-        cursor.execute(sql, symbol )
-        result = cursor.fetchone()
-        company_id = result['company_id']
-
-        sql = "select article, article_date, article_link from news where company_id = %s"
-        cursor.execute( sql, company_id )
-        result = cursor.fetchall()
+        sql = 'select about from company_meta where company_id = %s'
+        cursor.execute(sql, company_id )
+        about = cursor.fetchall()
+        about = about[0]['about']
+        cursor.close()
 
 
     # fetch earnings
 
-        
+
+    # get RSI
+    rsi = ''
+    url = f'https://api.polygon.io/v1/indicators/rsi/{symbol}?timespan=minute&adjusted=true&window=14&series_type=close&order=desc&limit=1&apiKey={api_key}'
+    r = requests.get( url )
+    rsi = r.json()['results']['values'][0]['value'] 
+
+    if 'SELL' in recommendation and int(rsi) > 70:
+        color = 'red'
+        ema_direction = recommendation = 'SELL'
+    elif 'BUY' in recommendation and int(rsi) < 30:
+        color = 'green'
+        ema_direction = recommendation = 'BUY'
+    else: 
+        color = 'grey'
+        ema_direction = recommendation = 'NEUTRAL'
+
+    print( ema_direction, flush=True)
+
+    #insert ema into database
+    with connection.cursor() as cursor:
+       cursor.execute('select * from ema_cross where company_id = %s', (company_id,))
+       current_ema = cursor.fetchone()
+
+       if current_ema == None: 
+          sql = 'insert into ema_cross ( ema_direction, company_id ) values ( %s, %s )'
+          val = ( ema_direction, company_id )
+          cursor.execute(sql, val )
+          connection.commit()
+    cursor.close()
+
+    # add RSI value to database
+    with connection.cursor() as cursor:
+        cursor.execute('select * from rsi where company_id = %s', (company_id,))
+        current_ema = cursor.fetchone()
+        sql = 'insert into rsi ( rsi_5, company_id ) values ( %s, %s )'
+        val = ( rsi, company_id )
+        cursor.execute(sql, val )
+        connection.commit()
+        cursor.close() 
+
+
+    # check sentiment
+
 
     # Render the template and pass the chart data to it
-    return render_template('ticker.html', recommendation=recommendation, color=color, data=result, info=indicators, ticker=symbol, tv_url=tv_url, exchange=exchange)
+    return render_template('ticker.html', recommendation=recommendation, color=color, data=result, ticker=symbol, tv_url=tv_url, exchange=exchange, last_ema13=round(last_ema13,2), last_ema48=round(last_ema48,2), rsi=round(rsi,2), about=about )
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
+        user = cursor.fetchone()
+        cursor.close()
+        if user and check_password_hash(user['password'], password):
+            session['email'] = email
+            session['logged_in']=True
+            return redirect('/dashboard')
+        else:
+            flash('Invalid email or password.')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO users (email, password) VALUES (%s, %s)', (email, hashed_password))
+        connection.commit()
+        cursor.close()
+        flash('Registration successful. Please login.')
+        return redirect('/login')
+    return render_template('register.html')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    try:
+      if session['logged_in']==False:
+         flash("You must be logged in to access this page")
+         return redirect('/login')
+      elif session['logged_in']==True: 
+         return render_template('dashboard.html')
+    except:
+      return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 #@app.errorhandler(Exception)
 def error(e):
@@ -139,4 +227,4 @@ def error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True, port=5000, host='0.0.0.0')
