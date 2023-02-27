1f2832ebaa930cb951a3c20deec495f3fb25952e
Sam Hagin <samhagin@yahoo.com>
2023-02-26 20:43:44 -0700

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,231 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: capstone
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `company` (
+  `company_id` int NOT NULL AUTO_INCREMENT,
+  `name` longtext NOT NULL,
+  `info_url` longtext NOT NULL,
+  PRIMARY KEY (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=504 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `company_meta`
+--
+
+DROP TABLE IF EXISTS `company_meta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `company_meta` (
+  `meta_id` int NOT NULL AUTO_INCREMENT,
+  `company_id` int NOT NULL,
+  `about` longtext,
+  PRIMARY KEY (`meta_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `company_meta_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=504 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dividends`
+--
+
+DROP TABLE IF EXISTS `dividends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dividends` (
+  `dividends_id` int NOT NULL AUTO_INCREMENT,
+  `dividends_date` datetime DEFAULT NULL,
+  `company_id` int NOT NULL,
+  PRIMARY KEY (`dividends_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `dividends_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `earnings`
+--
+
+DROP TABLE IF EXISTS `earnings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `earnings` (
+  `earnings_id` int NOT NULL AUTO_INCREMENT,
+  `earnings_date` datetime DEFAULT NULL,
+  `company_id` int NOT NULL,
+  PRIMARY KEY (`earnings_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `earnings_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ema_cross`
+--
+
+DROP TABLE IF EXISTS `ema_cross`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ema_cross` (
+  `ema_id` int NOT NULL AUTO_INCREMENT,
+  `ema_direction` varchar(255) DEFAULT NULL,
+  `company_id` int NOT NULL,
+  PRIMARY KEY (`ema_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `ema_cross_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=502 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `news` (
+  `news_id` int NOT NULL AUTO_INCREMENT,
+  `company_id` int NOT NULL,
+  `article` longtext,
+  `article_date` text,
+  `article_link` longtext,
+  PRIMARY KEY (`news_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `news_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=504 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `rsi`
+--
+
+DROP TABLE IF EXISTS `rsi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rsi` (
+  `rsi_id` int NOT NULL AUTO_INCREMENT,
+  `rsi_5` float DEFAULT NULL,
+  `company_id` int NOT NULL,
+  PRIMARY KEY (`rsi_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `rsi_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1816 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ticker`
+--
+
+DROP TABLE IF EXISTS `ticker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticker` (
+  `ticker_id` int NOT NULL AUTO_INCREMENT,
+  `company_id` int NOT NULL,
+  `symbol` varchar(10) DEFAULT NULL,
+  `prevDay` float DEFAULT NULL,
+  `day` float DEFAULT NULL,
+  `change` float DEFAULT NULL,
+  PRIMARY KEY (`ticker_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `ticker_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=504 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary view structure for view `ticker_list`
+--
+
+DROP TABLE IF EXISTS `ticker_list`;
+/*!50001 DROP VIEW IF EXISTS `ticker_list`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `ticker_list` AS SELECT 
+ 1 AS `company_id`,
+ 1 AS `name`,
+ 1 AS `symbol`,
+ 1 AS `info_url`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `email` longtext NOT NULL,
+  `password` longtext NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `watchlist`
+--
+
+DROP TABLE IF EXISTS `watchlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `watchlist` (
+  `watch_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `ticker` text NOT NULL,
+  PRIMARY KEY (`watch_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `ticker_list`
+--
+
+/*!50001 DROP VIEW IF EXISTS `ticker_list`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `ticker_list` AS select `company`.`company_id` AS `company_id`,`company`.`name` AS `name`,`ticker`.`symbol` AS `symbol`,`company`.`info_url` AS `info_url` from (`company` join `ticker` on((`company`.`company_id` = `ticker`.`company_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-27  3:42:01
